8a1056c2a48cea3343c23c53100c65c2e11c64de
AlisonYoon <politescarlett1@gmail.com>
2019-07-31 16:25:30 +0100

    START END, DECLARE

--- transactions.sql
+++ transactions.sql
@@ -12,6 +12,17 @@ Compliance in Database
 MYsql creates a ghost table (copied table) to update a table. That ghost table exist while transaction is happening,
 when transaction is finished, ghost table becomes the table everyone can see.
 
+
+---  in the query ------------
+START TRANSACTION
+DECLARE @aVariable=/*statement A*/;  --using @ in front of variable name prevents sequal injection
+/*statement B (using aVariable)*/
+/*a statement*/
+/*another statement*/
+
+END TRANSACTION
+---------------
+
 ==================
 
 <Storage Engine>
@@ -53,4 +64,27 @@ One-to-One (adult table ID and child table ID)
 Many-to-One : foreign key constraint
 One-to-Many (adult table ID and child1, child2....) : same thing from a different perspective
 Many-to-Many : linking table
+
+==================
+
+<Other Databases>
+
+EAV table - Entity, Attribute, Value
+
+Table UID (Unique IDentifier)
+When a table is created or added to a database, an automatic and unique identifier (UID) is associated with the table. A table UID is unique within the database associated with the table.
+A table UID is an unsigned long value that can be used as an alternative method to operate on tables, once the table is created or added to a database.
+
+==================
+
+<NoSQL>
+Not Only Structured Query Language
+
+Graph DB
+
+
+
+Document DB
+-Mongo DB
+
 
