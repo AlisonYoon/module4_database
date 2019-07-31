05c5feb19abe344a83611d0643adc92edefedfcd
AlisonYoon <politescarlett1@gmail.com>
2019-07-31 15:35:29 +0100

    relationship types, temp table, storage engine

--- transactions.sql
+++ transactions.sql
@@ -12,4 +12,45 @@ Compliance in Database
 MYsql creates a ghost table (copied table) to update a table. That ghost table exist while transaction is happening,
 when transaction is finished, ghost table becomes the table everyone can see.
 
+==================
+
+<Storage Engine>
+
+MySQL = database engine
+
+Storage engine (is part of database engine)
+    -InnoDB
+    -MyISAM
+
+'InnoDB' :
+Row-level locking (queries cannot be run agasint rows while that row is being updated. If someone else tries to update a row I am working on, it will skip that row do other things and come back to that row after I finish)
+Has transactions
+Has foreign keys
+Fast for reading   (good for something like account of people)
+
+'MyISAM' :
+Table-level locking (queries cannot be run agasint tables while that table is being updated.)
+No transactions
+No foreign keys  (MyIsam does not allow foreign key)
+Fast for writing  (good for something like logging archive)
+
+
+==================
+
+<Temp Tables>
+
+CREATE TEMPORARY TABLE `something`;
+
+this is a manually created ghost table. You can see it in the result of  query but it won't' appear on the left sidebar in the
+sequalPro.
+
+
+==================
+
+<Relationship Types>
+
+One-to-One (adult table ID and child table ID)
+Many-to-One : foreign key constraint
+One-to-Many (adult table ID and child1, child2....) : same thing from a different perspective
+Many-to-Many : linking table
 
