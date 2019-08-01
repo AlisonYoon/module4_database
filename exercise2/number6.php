c6525d4b0121dce02dbed843990dec03b3b126da
AlisonYoon <politescarlett1@gmail.com>
2019-08-01 14:35:26 +0100

    exercise2 number 6 done. kind of. partially working but stopping here because it got boring

--- exercise2/number6.php
+++ exercise2/number6.php
@@ -11,19 +11,22 @@ if(isset($_GET['name']) && isset($_GET['dob']) && isset($_GET['gender'])) {
     $dobToAdd = $_GET['dob'];
     $genderToAdd = $_GET['gender'];
     //Add in Children Table
-    $childNameToAdd = $_GET['childName'];
-    $childDobToAdd = $_GET['childDob'];
-    $childGenderToAdd = $_GET['childGender'];
-    //Add in Parent_of Table
-    $adultIDToAdd = $_GET['adultID'];
-    $childIDToAdd = $_GET['childID'];
+    $childNameToAdd1 = $_GET['child1Name'];
+    $childDobToAdd1 = $_GET['child1Dob'];
+    $childColorToAdd1 = $_GET['favColor1'];
+    //child 2
+    $childNameToAdd2 = $_GET['child2Name'];
+    $childDobToAdd2 = $_GET['child2Dob'];
+    $childColorToAdd2 = $_GET['favColor2'];
+    //child 3
+    $childNameToAdd3 = $_GET['child3Name'];
+    $childDobToAdd3 = $_GET['child3Dob'];
+    $childColorToAdd3 = $_GET['favColor3'];
+
     //Add in Pets Table
     $typeToAdd = $_GET['type'];
-    $belongsToToAdd = $_GET['belongsTo'];
 
-
-
-
+    
     $sql = $db->prepare('INSERT INTO `adults` (`name`, `DOB`, `gender`) VALUES (:name , :dob , :gender);');
     $sql->bindParam('name', $nameToAdd, PDO::PARAM_STR);
     $sql->bindParam('dob', $dobToAdd, PDO::PARAM_STR);
@@ -31,6 +34,62 @@ if(isset($_GET['name']) && isset($_GET['dob']) && isset($_GET['gender'])) {
 
     $sql->execute();
 
+    if(isset($_GET['child1Name']) && isset($_GET['child1Dob']) && isset($_GET['favColor1']) || isset($_GET['type'])) {
+        //Retrieve new adult's id
+        $newAdult = $db->prepare('SELECT `id` FROM `adults` ORDER BY `id` DESC LIMIT 1');
+        $newAdult->execute();
+        $newAdultId = $newAdult->fetch();
+        $newAdultId =  $newAdultId["id"];
+
+        //Add first child in the Children Table
+        $newChild1 = $db->prepare('INSERT INTO `children` (`name`, `DOB`, `f_color`) VALUES (:child1Name, :child1Dob, :favColor1);');
+        $newChild1->bindParam('child1Name', $childNameToAdd1, PDO::PARAM_STR);
+        $newChild1->bindParam('child1Dob', $childDobToAdd1, PDO::PARAM_STR);
+        $newChild1->bindParam('favColor1', $childColorToAdd1, PDO::PARAM_INT);
+
+        $newChild1->execute();
+
+        //Add a pet in the Pets Table
+        $newPet = $db->prepare('INSERT INTO `pets` (`type`, `belongs_to`) VALUES (:type, :belongs_to)');
+        $newPet->bindParam('type', $typeToAdd, PDO::PARAM_STR);
+        $newPet->bindParam('belongs_to', $newAdultId, PDO::PARAM_INT);
+
+        $newPet->execute();
+
+        //Retrieve Child1's id
+        $newChildAdded = $db->prepare('SELECT `id` FROM `children` ORDER BY `id` DESC LIMIT 1');
+        $newChildAdded ->execute();
+        $newChildId1  = $newChildAdded->fetch();
+        $newChildId1  =  $newChildId1["id"];
+
+        //Update Parent_of Table
+        $parentOf1 = $db->prepare('INSERT INTO `parent_of` (`adults_id`, `children_id`) VALUES (:adults_id, :children_id);');
+        $parentOf1->bindParam('adults_id', $newAdultId, PDO::PARAM_INT);
+        $parentOf1->bindParam('children_id', $newChildId1, PDO::PARAM_INT);
+
+        $parentOf1->execute();
+
+        if(isset($_GET['child2Name']) && isset($_GET['child2Dob']) && isset($_GET['favColor2'])) {
+            $newChild2 = $db->prepare('INSERT INTO `children` (`name`, `DOB`, `f_color`) VALUES (:child2Name, :child2Dob, :favColor2);');
+            $newChild2->bindParam('child2Name', $childNameToAdd2, PDO::PARAM_STR);
+            $newChild2->bindParam('child2Dob', $childDobToAdd2, PDO::PARAM_STR);
+            $newChild2->bindParam('favColor2', $childColorToAdd2, PDO::PARAM_INT);
+
+            $newChild2->execute();
+
+            if(isset($_GET['child3Name']) && isset($_GET['child3Dob']) && isset($_GET['favColor3'])) {
+                $newChild3 = $db->prepare('INSERT INTO `children` (`name`, `DOB`, `f_color`) VALUES (:child3Name, :child3Dob, :favColor3);');
+                $newChild3->bindParam('child3Name', $childNameToAdd3, PDO::PARAM_STR);
+                $newChild3->bindParam('child3Dob', $childDobToAdd3, PDO::PARAM_STR);
+                $newChild3->bindParam('favColor3', $childColorToAdd3, PDO::PARAM_INT);
+
+                $newChild3->execute();
+
+            }
+        }
+
+    }
+    
 }
 ?>
 
@@ -61,28 +120,36 @@ if(isset($_GET['name']) && isset($_GET['dob']) && isset($_GET['gender'])) {
         <label for="gender">Adult Gender(f/m)</label>
         <input type="text" name="gender">
     </div>
-    <p>Add a child in the table</p>
-    <div class="child">
-        <label for="childName">Child Name</label>
-        <input type="text" name="childName">
-        <label for="childDob">child Date of Birth(yyyy-mm-dd)</label>
-        <input type="text" name="childDob">
-        <label for="childGender">child Gender(f/m)</label>
-        <input type="text" name="childGender">
+    <p>Add children</p>
+    <div class="child1">
+        <label for="child1Name">Child Name</label>
+        <input type="text" name="child1Name">
+        <label for="child1Dob">child Date of Birth(yyyy-mm-dd)</label>
+        <input type="text" name="child1Dob">
+        <label for="favColor1">child favorite color (1~11)</label>
+        <input type="text" name="favColor1">
     </div>
-    <p>Add a parent-child relationship in the table</p>
-    <div class="parent-child">
-        <label for="adultID">Adult ID</label>
-        <input type="number" name="adultID">
-        <label for="childID">Children ID</label>
-        <input type="number" name="childID">
+    <div class="child2">
+        <label for="child2Name">Child Name</label>
+        <input type="text" name="child2Name">
+        <label for="child2Dob">child Date of Birth(yyyy-mm-dd)</label>
+        <input type="text" name="child2Dob">
+        <label for="favColor2">child favorite color (1~11)</label>
+        <input type="text" name="favColor2">
     </div>
-    <p>Add a pet in the table</p>
+    <div class="child3">
+        <label for="child3Name">Child Name</label>
+        <input type="text" name="child3Name">
+        <label for="child3Dob">Child Date of Birth(yyyy-mm-dd)</label>
+        <input type="text" name="child3Dob">
+        <label for="favColor3">child favorite color (1~11)</label>
+        <input type="text" name="favColor3">
+    </div>
+
+    <p>Add a pet</p>
     <div class="pet">
         <label for="type">Pet Type</label>
         <input type="text" name="type">
-        <label for="belongsTo">Belongs To (Adult Id)</label>
-        <input type="number" name="belongsTo">
     </div>
     <input type="submit">
 </form>
