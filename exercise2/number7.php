3e775eacf6eac3f8fe5d6b31cef93d487384b80d
AlisonYoon <politescarlett1@gmail.com>
2019-08-04 19:33:56 +0100

    works! done

--- exercise2/number7.php
+++ exercise2/number7.php
@@ -1,22 +1,22 @@
 <?php
 
-if(isset($_GET['f_color'])) {
+   if(isset($_GET['f_color'])) {
 
-$db = new PDO('mysql:host=192.168.20.20; dbname=ex2', 'root', '');
-$db->setAttribute(
-   PDO::ATTR_DEFAULT_FETCH_MODE,
-   PDO::FETCH_ASSOC
-);
+   $db = new PDO('mysql:host=192.168.20.20; dbname=ex2', 'root', '');
+   $db->setAttribute(
+      PDO::ATTR_DEFAULT_FETCH_MODE,
+      PDO::FETCH_ASSOC
+   );
 
-$favClrToGet = $_GET['f_color'];
+   $favClrToGet = $_GET['f_color'];
 
-$favClr = $db->prepare('SELECT `id` FROM `children` WHERE `f_color` =:f_color');
-$favClr->bindParam('f_color', $favClrToGet, PDO::PARAM_STR);
-$favClr->execute();
-$childId = $favClr->fetch();
+   $favClr = $db->prepare('SELECT type FROM pets INNER JOIN parent_of ON pets.belongs_to = parent_of.adults_id JOIN children ON children.id = parent_of.children_id WHERE children.f_color =:f_color; ');
+   $favClr->bindParam('f_color', $favClrToGet, PDO::PARAM_INT);
+   $favClr->execute();
+   $childId = $favClr->fetchAll();
 
-var_dump($childId);
-
+   
+   var_dump($childId);
 
 }
 
@@ -27,7 +27,7 @@ var_dump($childId);
 
 <form action="number7.php" method="get">
     <label for="f_color">Favorite color (1~11)</label>
-    <input type="text" name="f_color">
+    <input type="number" name="f_color">
     <input type="submit">
 </form>
 </body>
